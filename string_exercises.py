5be264f3f86e2d72bbef476e2c9598af4bf0d7d4
Conner Homan <connermhoman@gmail.com>
2024-01-31 13:54:14 -0500

    Add files via upload

--- string_exercises.py
+++ string_exercises.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Do not use reserved keywords for variable names
 
 print("Hello World")
@@ -27,4 +28,35 @@ print(student_name + ": " + str(score))
 print("Name: {} \nScore: {}".format (student_name, score))
 # f-strings
 print(f'Name: {student_name} \nScore: {score}')
+=======
+# Do not use reserved keywords for variable names
+
+print("Hello World")
+
+
+##### STRING SLICING #####
+emp_name = "Jane Doe"
+print(emp_name[2:6])
+print(emp_name[:4])
+print(emp_name[4:])
+print(emp_name[-4:-1])
+print(emp_name[1:6:2])
+
+print(emp_name.count('e'))
+
+print(emp_name.find('Doe'))
+
+emp_name = (emp_name.replace('Jane', 'John'))
+# Membership Test
+print('oh' in emp_name)
+
+#String Formatting
+student_name = "Alice"
+score = 87
+
+print(student_name + ": " + str(score))
+print("Name: {} \nScore: {}".format (student_name, score))
+# f-strings
+print(f'Name: {student_name} \nScore: {score}')
+>>>>>>> d889835ed4e1721c0d147e6435201e1b23b94fc6
 print(f'10 times 3 is {10*3}')
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-print("Hello world!")
-print("How are you?")
-print("Learning diff")
-print("Hello from dev")
+radius = int(input("What is the radius? "))
+pi = 3.14
+area_of_circle = pi * radius
+print("The area is")
+print(area_of_circle)
